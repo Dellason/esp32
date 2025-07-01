0a7fe8706df11aa2bcd1e2c48e07d5979c816e79
Dellason <dellasonjessica@gmail.com>
2025-07-01 15:07:31 +0000

    Edited

--- motor_move/motor_move.ino
+++ motor_move/motor_move.ino
@@ -1,34 +1,59 @@
-#define LEFT_REAR 16     // IN3
-#define LEFT_FRONT 17    // IN4
-#define RIGHT_REAR 19    // IN1
-#define RIGHT_FRONT 18   // IN2
+//esp32 Movement
 
+#define LEFT_REVERSE 16     
+#define LEFT_FORWARD 17    
+#define RIGHT_FORWARD 19    
+#define RIGHT_REVERSE 18  
 
 void setup() {
-  pinMode(LEFT_REAR, OUTPUT);
-  pinMode(LEFT_FRONT, OUTPUT);
-  pinMode(RIGHT_REAR, OUTPUT);
-  pinMode(RIGHT_FRONT, OUTPUT);
+  pinMode(LEFT_REVERSE, OUTPUT);
+  pinMode(LEFT_FORWARD, OUTPUT);
+  pinMode(RIGHT_REVERSE, OUTPUT);
+  pinMode(RIGHT_FORWARD, OUTPUT);
+  serial.Begin(115200);
 }
 
+void stopMotors() {
+  digitalWrite(LEFT_FORWARD, LOW);
+  digitalWrite(RIGHT_FORWARD, LOW);
+  digitalWrite(LEFT_REVERSE, LOW);
+  digitalWrite(RIGHT_REVERSE, LOW);
+}
+
+
 void loop() {
-  // FORWARD
-
-  // RIGHT MOTOR: forward
-  digitalWrite(RIGHT_REAR, HIGH);
-  digitalWrite(RIGHT_FRONT, LOW);
-
-  // LEFT MOTOR
-  digitalWrite(LEFT_REAR, LOW);
-  digitalWrite(LEFT_FRONT, HIGH);
-
+  //FORWARD
+  digitalWrite(LEFT_FORWARD, HIGH);
+  digitalWrite(RIGHT_FORWARD, HIGH);
   delay(1000);
 
-  // STOP
-  digitalWrite(LEFT_REAR, LOW);
-  digitalWrite(LEFT_FRONT, LOW);
-  digitalWrite(RIGHT_REAR, LOW);
-  digitalWrite(RIGHT_FRONT, LOW);
+  //STOP
+  digitalWrite(LEFT_FORWARD, LOW);
+  digitalWrite(RIGHT_FORWARD, LOW);
+  delay(500);
 
-  delay(5000);
+  //REVERSE
+  digitalWrite(LEFT_REVERSE, HIGH);
+  digitalWrite(RIGHT_REVERSE, HIGH);
+  delay(1000);
+
+  //TURN LEFT
+  digitalWrite(RIGHT_FORWARD, HIGH);
+  digitalWrite(LEFT_REVERSE, HIGH);
+  delay(600);
+
+  //STOP
+  digitalWrite(LEFT_REVERSE, LOW);
+  digitalWrite(RIGHT_FORWARD, LOW);
+  delay(500);
+
+   //TURN RIGHT
+  digitalWrite(LEFT_FORWARD, HIGH);
+  digitalWrite(RIGHT_REVERSE, HIGH);
+  delay(500);
+
+  //STOP
+  digitalWrite(LEFT_FORWARD, LOW);
+  digitalWrite(RIGHT_REVERSE, LOW);
+  delay(1000);
 }
